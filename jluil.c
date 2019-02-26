74673d8e17143b72667b7fe7c27d0e63a2667623
NihasAhamed <43338603+NihasAhamed@users.noreply.github.com>
2019-02-26 12:13:29 +0530

    Create jluil.c

---
+++ jluil.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s[100];
+int i;
+clrscr();
+printf("Enter the string:");
+gets(s);
+if(!isdigit(s[0]))
+printf("True");
+else
+printf("False");
+getch();
+}
